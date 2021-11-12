0d8085b8c131273122d2b588deb12444b392abdb
Sh Maruf <74917669+shmaruf58@users.noreply.github.com>
2021-11-12 20:41:20 +0600

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,3 +1,23 @@
+
+
+
+// Preloader
+
+window.addEventListener("load", function () {
+  document.querySelector(".preloader").classList.add("opacity-0");
+  setTimeout(function () {
+    document.querySelector(".preloader").style.display = "none";
+  }, 200);
+});
+
+
+
+
+
+
+
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
